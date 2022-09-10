e0b5baeaae86d7f4d2fe1f68305a0d94ab113f0f
Daniel <daniel.brownell@gmail.com>
2022-09-10 11:23:03 +0200

    Revert "Updated with ReasSense and Lidar pollers"
    This reverts commit f8f0aff10ecd728de2fa3309c47ff52a85907262.

--- webapp/config.py
+++
@@ -1,8 +0,0 @@
-settings = dict(
-
-    lidar = True,
-    robot = True,
-    arduino_115200 = False,
-    realsense = False,
-    realsense_url = 'http://192.168.1.169:5000/distances'
-)
--- webapp/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-gunicorn --bind 0.0.0.0 --worker-class eventlet -w 1 kmp8servo:app
-
