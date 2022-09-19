a96d4999d02e14fe8997974c860cb1b38d7cb7fb
Daniel <daniel.brownell@gmail.com>
2022-09-19 18:16:55 +0200

    Update config.py
    Added new options for gripper

--- webapp/config.py
+++ webapp/config.py
@@ -5,5 +5,7 @@ settings = dict(
     arduino_115200 = False,
     realsense = False,
     realsense_url = 'http://192.168.1.169:5000/distances',
-    picamera = False
+    picamera = False,
+    continuous_servo_on_pin_25 = False,
+    has_switches_on_pins_23_34 = False
 )
