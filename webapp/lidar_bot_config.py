7e97cb2346a82aefaf674c410fee7bbb7a9c9e0c
javadan <daniel.brownell@gmail.com>
2022-10-08 14:36:54 +0100

    adding lidarbot default motions

---
+++ webapp/lidar_bot_config.py
@@ -0,0 +1,16 @@
+settings = dict(
+    lidar = True,
+    pca9685_robot = True,
+    scservo_robot = False,
+    gpio_on_5_6_13_19_robot = False,
+    arduino_115200 = False,
+    realsense = False,
+    realsense_url = 'http://192.168.1.169:5000/distances',
+    picamera = False,
+    libcamera = False,
+    streaming = False,
+    ultrasound_echo_on_pin_22 = False,
+    ultrasound_trigger_on_pin_27 = False,
+    continuous_servo_on_pin_25 = False,
+    has_switches_on_pins_23_34 = False
+)
